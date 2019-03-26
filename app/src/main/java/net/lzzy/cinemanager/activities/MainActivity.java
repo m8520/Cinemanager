efcc1cd892865df04a2cd378deff55c097a72864
morupeng <1363583178@qq.com>
2019-03-26 15:49:22 +0800

    自定义标题栏 2019/3/26 第九节课

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -2,18 +2,68 @@ package net.lzzy.cinemanager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import net.lzzy.cinemanager.R;
+import net.lzzy.cinemanager.models.Cinema;
+import net.lzzy.cinemanager.models.Order;
+import net.lzzy.cinemanager.models.OrderFactory;
+import net.lzzy.simpledatepicker.CustomDatePicker;
+import net.lzzy.sqllib.GenericAdapter;
+
+import java.util.List;
 
 /**
  * @author Administrator
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private LinearLayout layoutMenu;
+    private TextView tvTitle;
+    private SearchView search;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /** 去掉标题栏 **/
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        setTitleMenu();
     }
-}
+
+    /** 标题栏 **/
+    private void setTitleMenu() {
+        layoutMenu = findViewById(R.id.bar_title_layout_menu);
+        layoutMenu.setVisibility(View.GONE);
+        findViewById(R.id.bar_title_img_menu).setOnClickListener(this);
+        tvTitle = findViewById(R.id.bar_title_tv_title);
+        tvTitle.setText(R.string.bar_title_menu_orders);
+        search = findViewById(R.id.main_sv_search);
+        findViewById(R.id.bar_title_tv_add_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_view_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_add_order).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_view_order).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_exit).setOnClickListener(this);
+    }
+
+
+    /** 对标题栏的点击监听 **/
+    @Override
+    public void onClick(View v) {
+
+    }}
+
