ce88b8bfa9d7d4a7e82367f330ee8b4fe19f53a5
morupeng <1363583178@qq.com>
2019-03-27 12:27:59 +0800

    视图列表并显示 2019/3/27 第5节课

--- app/src/main/java/net/lzzy/cinemanager/framents/AddCinemasFragment.java
+++ app/src/main/java/net/lzzy/cinemanager/framents/AddCinemasFragment.java
@@ -1,30 +1,81 @@
 package net.lzzy.cinemanager.framents;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.lljjcoder.Interface.OnCityItemClickListener;
+import com.lljjcoder.bean.CityBean;
+import com.lljjcoder.bean.DistrictBean;
+import com.lljjcoder.bean.ProvinceBean;
+import com.lljjcoder.style.cityjd.JDCityPicker;
+
 import net.lzzy.cinemanager.R;
+import net.lzzy.cinemanager.models.Cinema;
 
 /**
  * Created by lzzy_gxy on 2019/3/27.
  * Description:
  */
-public class AddCinemasFragment extends Fragment {
-    public AddCinemasFragment(){}
+public class AddCinemasFragment extends BaseFragment {
+    private String province="广西壮族自治区";
+    private String city="柳州市";
+    private String area="鱼峰区";
+    private TextView tvArea;
+    private EditText edtName;
 
-
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_cinemas,null);
-        TextView textView=view.findViewById(R.id.fragment_addOrder_tv);
-        return view;
+    protected void populate() {
+        tvArea = find(R.id.dialog_add_cinema_btn_save);
+        edtName = find(R.id.dialog_add_cinema_edt_name);
+        find(R.id.dialog_add_cinema_layout_area).setOnClickListener(v -> {
+            JDCityPicker cityPicker = new JDCityPicker();
+            cityPicker.init(getActivity());
+            cityPicker.setOnCityItemClickListener(new OnCityItemClickListener() {
+                @Override
+                public void onSelected(ProvinceBean province, CityBean city, DistrictBean district) {
+                    AddCinemasFragment.this.province = province.getName();
+                    AddCinemasFragment.this.city = city.getName();
+                    AddCinemasFragment.this.area = district.getName();
+                    String loc = province.getName()+city.getName()+district.getName();
+                    tvArea.setText(loc);
+                }
+
+            });
+            cityPicker.showCityPicker();
+        });
+        find(R.id.dialog_add_cinema_btn_save).setOnClickListener(v -> {
+            String name=edtName.getText().toString();
+            if(TextUtils.isEmpty(name)){
+                Toast.makeText(getActivity(),"要有名称",Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Cinema cinema=new Cinema();
+            cinema.setName(name);
+            cinema.setArea(area);
+            cinema.setCity(city);
+            cinema.setProvince(province);
+            cinema.setLocation(tvArea.getText().toString());
+            edtName.setText("");
+        });
+
+
     }
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.fragment_addcinemas;
+    }
+
+
+
 }
--- app/src/main/java/net/lzzy/cinemanager/framents/AddOrderFrament.java
+++ app/src/main/java/net/lzzy/cinemanager/framents/AddOrderFrament.java
@@ -16,15 +16,16 @@ import net.lzzy.cinemanager.R;
  * Created by lzzy_gxy on 2019/3/27.
  * Description:
  */
-public class AddOrderFrament extends Fragment {
-    public AddOrderFrament(){}
-
-    @Nullable
+public class AddOrderFrament extends BaseFragment {
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_cinemas,null);
-        TextView textView=view.findViewById(R.id.fragment_addCinemas_tv);
-        return view;
+    protected void populate() {
+
     }
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.fragment_addorder;
+    }
+
 
 }
